735126b4802aac9968f010fde94afad8462107db
jezdip1 <petr.jezdik@fel.cvut.cz>
2025-09-12 15:16:55 +0200

    ladime ensureSUL_LOG

--- matlab/train_pipeline.m
+++ matlab/train_pipeline.m
@@ -5,6 +5,7 @@ featCfg = jsondecode(fileread('./config/features_config.json'));
 mdlCfg  = jsondecode(fileread('./config/model_config.json'));
 T0 = azvpet.io.load_table(paths.raw_table);
 T1 = azvpet.features.ensure_features(T0, featCfg);
+
 [form, opts] = azvpet.model.define_formula(mdlCfg, T1);
 [M, info]    = azvpet.model.train_lmem(T1, form, opts);
 cv = azvpet.model.loo_cv(T1, form, opts);
