d1fd233e16d509a5dafb1135b99ad6b0292298bb
jezdip1 <petr.jezdik@fel.cvut.cz>
2025-09-12 13:05:44 +0200

    auto-merged matlab/startup.m

--- matlab/startup.m
+++ matlab/startup.m
@@ -1,17 +1,7 @@
 function startup
-  % --> Tohle je přesná cesta na složku 'matlab' (kde leží tento soubor)
+  % Spouštěj z kořene repa: run('matlab/startup.m')
   thisDir = fileparts(mfilename('fullpath'));   % .../AZV-PET/matlab
-
-  % Přidej JEN tuto složku (parent balíčku), ne package samotný
   addpath(thisDir);
-
-  % Ověření, že existuje package a klíčový soubor
-  pkgDir = fullfile(thisDir, '+azvpet');
-  assert(exist(pkgDir, 'dir')==7, 'Nenalezena složka balíčku: %s', pkgDir);
-
-  mdlFile = fullfile(pkgDir, 'model', 'train_lmem.m');
-  assert(exist(mdlFile, 'file')==2, 'Chybí soubor: %s', mdlFile);
-
   rehash path; rehash toolboxcache;
   fprintf('[startup] AZV-PET: added %s\n', thisDir);
 end
